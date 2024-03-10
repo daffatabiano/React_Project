a0bd027c6e69c27ef7eeb7d31b01bda920c5fbd7
daffa tabiano <daffatabianoo@gmail.com>
2024-03-10 23:30:38 +0700

    menambahkan style modal dan detail-user-> process ambil data detail dari api

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,27 +1,52 @@
-import axios from "axios";
-import "../style/home.css"
+import axios from 'axios';
+import '../style/modal.css';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const Modal = () => {
-  return (
-    <div>
-       <div className="modal fade" id="exampleModal" tabIndex="4" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div className="modal-body">
-        ...
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-  )
-}
+    const [user, setUser] = useState(null);
+    const { id } = useParams('');
 
-export default Modal
+    const getUserDetail = () => {
+        axios
+            .get(`https://reqres.in/api/users/${id}`)
+            .then((res) => {
+                setUser(res.data.data);
+                console.log(res?.data?.data);
+            })
+            .catch((err) => {
+                console.log(err.response);
+            });
+    };
+
+    useEffect(() => {
+        getUserDetail();
+    });
+    return (
+        <div>
+            <div
+                className="modal fade"
+                id="exampleModal"
+                tabIndex="4"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title " id="exampleModalLabel">
+                            </h1>
+                        </div>
+                        <div className="modal-body">
+                            <img src="" alt="" />
+                            <h4>content</h4>
+                        </div>
+                        <div className="modal-footer"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- src/page/DetailUser.jsx
+++ src/page/DetailUser.jsx
@@ -1,11 +1,44 @@
-import React from "react";
-
+import "../style/detail-user.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 const DetailUser = () => {
-  
+    const [user, setUser] = useState(null);
+    const  id  = useParams('')
 
-  return <div>
-     
-  </div>;
+    const getUserDetail = () => {
+        axios
+            .get(`https://reqres.in/api/users?${id}`)
+            .then((res) => {
+                setUser(res);
+                console.log(res?.data?.data);
+            })
+            .catch((err) => {
+                console.log(err.response);
+            })
+    }
+
+    useEffect(() => {
+        getUserDetail()
+    }, [])
+
+    return (
+        <div className="parents-detail">
+            <div className="card-detail">
+                <h1>Detail User</h1>
+                <img src="" alt="" />
+                <h3>Nama</h3>
+                <h2>Email</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+                    accusantium beatae voluptatum nostrum maxime blanditiis, est
+                    adipisci neque aut optio, veniam dolor delectus commodi
+                    deserunt fugiat fuga officiis quidem ipsa.
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default DetailUser;
